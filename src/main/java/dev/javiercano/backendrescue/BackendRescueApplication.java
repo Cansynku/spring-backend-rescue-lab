f61fa42160699f4da0f9cb941cfdd1f76ca36472
Cansynku <121675853+Cansynku@users.noreply.github.com>
2026-09-06 08:35:18 +0200

    chore: bootstrap Spring Boot backend rescue lab

---
+++ src/main/java/dev/javiercano/backendrescue/BackendRescueApplication.java
@@ -0,0 +1,11 @@
+package dev.javiercano.backendrescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRescueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendRescueApplication.class, args);
+    }
+}
